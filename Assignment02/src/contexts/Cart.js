12aede1566cc9ed4dcfb9444eb6e625e7563d8dd
Kat Amundson <kea1@iastate.edu>
2023-11-05 15:54:28 -0600

    updating quantity

--- Assignment02/src/contexts/Cart.js
+++ Assignment02/src/contexts/Cart.js
@@ -51,47 +51,117 @@ export const CartProvider = ({ children }) => {
     setTaxes(0);
   };
 
-  const addToCart = (item) => {
-    const newCart = [...cart, item];
+  // const addToCart = (item) => {
+  //   const newCart = [...cart, item];
+  //   const itemIndex = newCart.findIndex((cartItem) => cartItem.id === item.id);
 
-    let newSubtotal = newCart.reduce((acc, item) => acc + item.price, 0);
-    newSubtotal = Math.round(newSubtotal * 100) / 100;  
+  //   if (itemIndex >= 0) {
+  //     // If the item is already in the cart, increase its quantity
+  //     newCart[itemIndex].quantity += 1;
+  //   } else {
+  //     // Otherwise, add it to the cart with quantity 1
+  //     newCart.push({ ...item, quantity: 1 });
+  //   }
+
+  //   let newSubtotal = newCart.reduce((acc, item) => acc + item.price, 0);
+  //   newSubtotal = Math.round(newSubtotal * 100) / 100;  
+  //   const taxAmount = newSubtotal * TAX_RATE;
+  //   const newTotal = Math.round((newSubtotal + taxAmount) * 100) / 100;
+  
+  //   setSubtotal(newSubtotal);
+  //   setTotal(newTotal);
+  //   setTaxes(taxAmount);
+  //   setCart(newCart);
+  // };
+
+  // // const handleRemoveFromCart = (item) => {
+  // //   removeFromCart(item);
+  // //   setToastMessage(`Successfully removed ${item.name} from cart!`);
+  // //   setShowToast(true); // Use setShowToast
+  // //   setTimeout(() => setShowToast(false), 3000);
+  // // };
+
+  // const removeFromCart = (item) => {
+  //   setCart((prevCart) => {
+  //     const index = prevCart.lastIndexOf(item);
+  //     const itemIndex = newCart.findIndex((cartItem) => cartItem.id === item.id);
+
+  //     if (itemIndex >= 0) {
+  //       // If the item is in the cart, decrease its quantity
+  //       newCart[itemIndex].quantity -= 1;
+  //       if (newCart[itemIndex].quantity === 0) {
+  //         // If the quantity reaches zero, remove it from the cart
+  //         newCart.splice(itemIndex, 1);
+  //       }
+  //     }
+
+  //     if (index < 0) return prevCart;
+
+  //     const newCart = [
+  //       ...prevCart.slice(0, index),
+  //       ...prevCart.slice(index + 1, prevCart.length),
+  //     ];
+
+  //     const newSubtotal = newCart.reduce((acc, item) => acc + item.price, 0);
+  //     const taxAmount = newSubtotal * TAX_RATE;
+  //     setSubtotal(newSubtotal);
+  //     setTaxes(taxAmount);
+  //     setTotal(newSubtotal + taxAmount);
+
+  //     return newCart;
+  //   });
+  // };
+
+  const addToCart = (item) => {
+    const newCart = [...cart];
+    const itemIndex = newCart.findIndex((cartItem) => cartItem.id === item.id);
+  
+    if (itemIndex >= 0) {
+      // If the item is already in the cart, increase its quantity
+      newCart[itemIndex].quantity += 1;
+    } else {
+      // Otherwise, add it to the cart with quantity 1
+      newCart.push({ ...item, quantity: 1 });
+    }
+  
+    // Update subtotal, total, and taxes
+    const newSubtotal = newCart.reduce((acc, cartItem) => acc + cartItem.price * cartItem.quantity, 0);
     const taxAmount = newSubtotal * TAX_RATE;
-    const newTotal = Math.round((newSubtotal + taxAmount) * 100) / 100;
+    const newTotal = newSubtotal + taxAmount;
   
     setSubtotal(newSubtotal);
     setTotal(newTotal);
     setTaxes(taxAmount);
     setCart(newCart);
   };
-
-  const handleRemoveFromCart = (item) => {
-    removeFromCart(item);
-    setToastMessage(`Successfully removed ${item.name} from cart!`);
-    setShowToast(true); // Use setShowToast
-    setTimeout(() => setShowToast(false), 3000);
-  };
-
+  
   const removeFromCart = (item) => {
     setCart((prevCart) => {
-      const index = prevCart.lastIndexOf(item);
-
-      if (index < 0) return prevCart;
-
-      const newCart = [
-        ...prevCart.slice(0, index),
-        ...prevCart.slice(index + 1, prevCart.length),
-      ];
-
-      const newSubtotal = newCart.reduce((acc, item) => acc + item.price, 0);
+      const newCart = [...prevCart];
+      const itemIndex = newCart.findIndex((cartItem) => cartItem.id === item.id);
+  
+      if (itemIndex >= 0) {
+        // If the item is in the cart, decrease its quantity
+        newCart[itemIndex].quantity -= 1;
+        if (newCart[itemIndex].quantity === 0) {
+          // If the quantity reaches zero, remove it from the cart
+          newCart.splice(itemIndex, 1);
+        }
+      }
+  
+      // Update subtotal, total, and taxes
+      const newSubtotal = newCart.reduce((acc, cartItem) => acc + cartItem.price * cartItem.quantity, 0);
       const taxAmount = newSubtotal * TAX_RATE;
+      const newTotal = newSubtotal + taxAmount;
+  
       setSubtotal(newSubtotal);
       setTaxes(taxAmount);
-      setTotal(newSubtotal + taxAmount);
-
+      setTotal(newTotal);
+  
       return newCart;
     });
   };
+  
 
   return (
     <CartContext.Provider
