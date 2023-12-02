3d9188abe7f19c7d397a58ceb56162e48fcbc298
Kat Amundson <kea1@iastate.edu>
2023-12-02 14:40:24 -0600

    adding to index.js

--- team15_final/src/index.js
+++ team15_final/src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// init React :
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+// render something HTML :
+root.render(<h1>Hello World!</h1>);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
