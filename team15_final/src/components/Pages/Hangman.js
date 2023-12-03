83a2b7f9d7f4f2ea67a6166c0061f1e8b2a17643
neham <nmaddali@iastate.edu>
2023-12-03 17:48:53 -0600

    hangman with list of words react app

--- team15_final/src/components/Pages/Hangman.js
+++ team15_final/src/components/Pages/Hangman.js
@@ -1,203 +1,71 @@
-import React, { useState, useEffect } from "react";
-import canvasCreator from "./canvasCreator"; // Create a new file for the canvasCreator function
-import "./Hangman.css"; // Create a CSS file for your component styles
+import React, { useState, useEffect } from 'react';
+import Header from '../Header';
+import Figure from '../Figure';
+import WrongLetters from '../WrongLetters';
+import Word from '../Word';
+import Popup from '../Popup';
+import Notification from '../Notification';
+import { showNotification as show, checkWin } from '../helpers';
 
-export const Hangman = () => {
-  const [options, setOptions] = useState({
-    easy: "/easy.json",
-    medium: "/medium.json",
-    difficult: "/difficult.json",
-  });
+import './Hangman.css';
 
-  const [winCount, setWinCount] = useState(0);
-  const [count, setCount] = useState(0);
-  const [chosenWord, setChosenWord] = useState("");
-  const [disabledLetters, setDisabledLetters] = useState([]);
-  const [result, setResult] = useState("");
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
-    const fetchOptionsFromJSON = async (jsonFileName) => {
-      try {
-        const response = await fetch(jsonFileName);
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Error loading options from JSON files:", error);
-        return null;
-      }
-    };
-
-    const initializeHangman = async () => {
-      setWinCount(0);
-      setCount(0);
-      setChosenWord("");
-
-      try {
-        // json case sensitive
-        const easyOptions = await fetchOptionsFromJSON(options.easy);
-        const mediumOptions = await fetchOptionsFromJSON(options.medium);
-        const difficultOptions = await fetchOptionsFromJSON(options.difficult);
-
-        setOptions({
-          easy: easyOptions,
-          medium: mediumOptions,
-          difficult: difficultOptions,
-        });
-      } catch (error) {
-        console.error("Error initializing Hangman:", error);
-      }
-    };
-
-    initializeHangman();
-  }, [options]);
-
-  const handleUserInput = (input) => {
-    if (chosenWord) {
-      const charArray = chosenWord.split("");
-
-      if (!disabledLetters.includes(input)) {
-        if (charArray.includes(input.toUpperCase())) {
-          charArray.forEach((char, index) => {
-            if (char === input.toUpperCase()) {
-              // Update state instead of directly manipulating the DOM
-              setWinCount((prevWinCount) => prevWinCount + 1);
-              if (winCount === charArray.length - 1) {
-                setResult(`You Win!! The word was ${chosenWord}`);
-              }
-            }
-          });
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
         } else {
-          // Update state instead of directly manipulating the DOM
-          setCount((prevCount) => prevCount + 1);
-          if (count === 5) {
-            setResult(`You Lose!! The word was ${chosenWord}`);
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
           }
         }
-
-        // Update state instead of directly manipulating the DOM
-        setDisabledLetters([...disabledLetters, input]);
       }
     }
-  };
+    window.addEventListener('keydown', handleKeydown);
 
-  const generateWord = (optionValue) => {
-    let optionArray = options[optionValue];
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
 
-    if (optionArray && optionArray.length > 0) {
-      // Update state instead of directly manipulating the DOM
-      setChosenWord(
-        optionArray[Math.floor(Math.random() * optionArray.length)]
-      );
-      setChosenWord((prevChosenWord) => prevChosenWord.toUpperCase());
-    } else {
-      // Update state instead of directly manipulating the DOM
-      setResult("No words available for this difficulty.");
-    }
-  };
+  function playAgain() {
+    setPlayable(true);
 
-  const drawMan = (count) => {
-    // Implement the drawing logic using the canvasCreator
-    // You can call the canvas drawing functions based on the count
-  };
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
 
   return (
-    <div className="container">
-      <div id="options-container">
-        <div id="letter-container" className="letter-container hide">
-          <div className="user-input-section"></div>
-          <canvas id="canvas"></canvas>
-          <div id="new-game-container" className="new-game-popup hide">
-            <div id="result-text"></div>
-            <button id="new-game-button">New Game</button>
-          </div>
-        </div>{" "}
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
-    </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
   );
-};
+}
 
-export default Hangman;
-
-/*
-import React from "react";
-
-export const Hangman = () => {
-  return (
-    <div className="container">
-      <div id="options-container">
-        <div id="letter-container" className="letter-container hide">
-          <div className="user-input-section"></div>
-          <canvas id="canvas">
-          </canvas>
-          <div id="new-game-container" className="new-game-popup hide">
-            <div id="result-text"></div>
-            <button id="new-game-button">New Game</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-*/
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import ReactDOM from 'react-dom';
-// import canvasCreator from './canvasCreator';
-// import './Hangman.css';
-
-// class Hangman extends React.Component {
-//   state = {
-
-//   };
-
-//   handleGameStart = () => {
-//     let difficulty = document.querySelector("#level_select");
-//     if (difficulty.value === "beginner") {
-//         this.setState({
-
-//         });
-//     }
-//     if (difficulty.value === "intermediate") {
-//         this.setState({
-
-//         });
-//     }
-//     if (difficulty.value === "expert") {
-//         this.setState({
-
-//         });
-//     }
-// }
-
-
-// render() {
-//   return (
-//       <div className="game">
-//           <div className="game-info">
-              
-//               <h4>Select a level and click "start"</h4>
-//               <span className="info">Level:
-//                   <select id="level_select">
-//                       <option value="beginner"> Beginner </option>
-//                       <option value="intermediate"> Intermediate </option>
-//                       <option value="expert"> Expert </option>
-//                   </select>
-//               </span>
-//               <button onClick={this.handleGameStart}>Start</button>
-//           </div>
-
-          
-          
-//       </div>
-//   );
-// }
-
-
-// }
-
-// ReactDOM.render(<Hangman />, document.getElementById('root'));
-
-// export default Hangman;
+export default App;
