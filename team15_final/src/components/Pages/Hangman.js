cd485188be713acb0605283c6709599eaeaa339d
neham <nmaddali@iastate.edu>
2023-12-03 20:41:17 -0600

    file update

--- team15_final/src/components/Pages/Hangman.js
+++ team15_final/src/components/Pages/Hangman.js
@@ -1,3 +1,101 @@
+import React, { useState, useEffect } from "react";
+import Header from "../Header";
+import Figure from "../Figure";
+import WrongLetters from "../WrongLetters";
+import Word from "../Word";
+import Popup from "../Popup";
+import Notification from "../Notification";
+import { showNotification as show, checkWin } from "../helpers";
+
+import "./Hangman.css";
+
+const API_KEY = "3cb59fd1-e7e6-4f9f-81ec-903db616156a"; // Replace with your actual Merriam-Webster API key
+const API_URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json?key=${API_KEY}`;
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+  const [selectedWord, setSelectedWord] = useState("");
+  const [wordFetched, setWordFetched] = useState(false);
+
+  const fetchRandomWord = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      const randomWord = data[Math.floor(Math.random() * data.length)];
+      setSelectedWord(randomWord.toLowerCase());
+      setWordFetched(true);
+    } catch (error) {
+      console.error("Error fetching random word:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomWord();
+  }, []);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+    setCorrectLetters([]);
+    setWrongLetters([]);
+    setWordFetched(false); // Reset wordFetched when playing again
+    fetchRandomWord();
+  }
+
+  console.log("Selected word before return:", selectedWord); // Add this line for debugging
+
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+    </>
+  );
+}
+
+export default App;
+
+/*
+
 import React, { useState, useEffect } from 'react';
 import Header from '../Header';
 import Figure from '../Figure';
@@ -54,6 +152,8 @@ function App() {
     selectedWord = words[random];
   }
 
+  
+
   return (
     <>
       <Header />
@@ -69,3 +169,4 @@ function App() {
 }
 
 export default App;
+*/
--- team15_final/src/components/Word.js
+++ team15_final/src/components/Word.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
 const Word = ({ selectedWord, correctLetters }) => {
-
   return (
     <div className="word">
-      {selectedWord.split('').map((letter, i) => {
-        return (
+      {selectedWord ? (
+        selectedWord.split('').map((letter, i) => (
           <span className="letter" key={i}>
-            {correctLetters.includes(letter) ? letter : ''}
+            {correctLetters.includes(letter) ? letter : '_'}
           </span>
-        )
-      })}
+        ))
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default Word
+export default Word;
