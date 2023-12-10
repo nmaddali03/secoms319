82d18231fff39adbbccbbf3f0ccc8d22682e98a6
Kat Amundson <kea1@iastate.edu>
2023-12-09 21:24:59 -0600

    working stuff

---
+++ team15_final/frontend/src/components/Pages/Crud.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import './Crud.css';
+
+function Crud() {
+  const [newWord, setNewWord] = useState('');
+  const [wordToDelete, setWordToDelete] = useState('');
+  const [wordList, setWordList] = useState([]);
+  const [showList, setShowList] = useState(false);
+
+  useEffect(() => {
+    fetchWord();
+  }, []);
+
+  async function fetchWord() {
+    try {
+      console.log('Fetching words...');
+      const response = await fetch('http://localhost:8081/listWords');
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      setWordList(data);
+    } catch (error) {
+      console.error('Error fetching words:', error);
+    }
+  }
+
+  async function addWord() {
+    try {
+      const response = await fetch('http://localhost:8081/listWords', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ word: newWord }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Added word:', data);
+        fetchWord();
+        setNewWord('');
+      } else {
+        console.error('Failed to add word. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error adding word:', error);
+    }
+  }
+
+  async function deleteWord() {
+    try {
+      const response = await fetch(`http://localhost:8081/listWords/${wordToDelete}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({word: wordToDelete}),
+      });
+  
+      if (response.ok) {
+        console.log('Word deleted successfully');
+        fetchWord();
+        setWordToDelete('');
+      } else {
+        const errorData = await response.json();
+        console.error('Failed to delete word. Error:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Error deleting word:', error);
+    }
+  }
+      
+  function showWordList() {
+    setShowList((prevShowList) => !prevShowList);
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">CRUD - Hangman Word Management</h1>
+      <section className="formSection">
+        <button onClick={showWordList} className="button showListButton">
+          Show Word List
+        </button>
+      </section>
+
+      {showList && (
+        <div>
+          <h3 className="wordList">Word List</h3>
+          <div className="wordList">
+            {wordList.map((word) => (
+              <div key={word._id} className="listItem">
+                {word.word}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+
+      <section className="formSection">
+        <label htmlFor="newWord">New Word: </label>
+        <input
+          type="text"
+          id="newWord"
+          value={newWord}
+          onChange={(e) => setNewWord(e.target.value)}
+          className="inputField"
+        />
+        <button onClick={addWord} className="button addWordButton">
+          Add Word
+        </button>
+      </section>
+
+      <section className="formSection">
+        <label htmlFor="wordToDelete">Word to Delete: </label>
+        <input
+          type="text"
+          id="wordTo aDelete"
+          value={wordToDelete}
+          onChange={(e) => setWordToDelete(e.target.value)}
+          className="inputField"
+        />
+        <button onClick={deleteWord} className="button deleteWordButton">
+          Delete Word
+        </button>
+      </section>
+    </div>
+  );
+}
+
+export default Crud;
--- team15_final/frontend/src/server.js
+++ team15_final/frontend/src/server.js
@@ -1,90 +1,103 @@
-var express = require("express");
-var cors = require("cors");
-var app = express();
-var fs = require("fs");
-var bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 
 const port = 8081;
 const host = "localhost";
 
-// Mongo
 const url = "mongodb://127.0.0.1:27017";
 const dbName = "team15_final";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
+app.listen(port, () => {
+  console.log("App listening at http://%s:%s", host, port);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello, this is the root path!");
+  res.send("Hello, World!");
 });
 
 app.get("/listWords", async (req, res) => {
   await client.connect();
   console.log("Node connected successfully to GET MongoDB");
+
   const query = {};
   const results = await db
     .collection("words")
     .find(query)
     .limit(100)
     .toArray();
+
   console.log(results);
   res.status(200).send(results);
 });
 
-app.get("/:id", async (req, res) => {
-    const wordid = Number(req.params.id);
-    console.log("Word to find :", wordid);
+app.get("/listWords/:word", async (req, res) => {
+  const wordName = req.params.word;
 
+  await client.connect();
+  console.log("Node connected successfully to GET-word MongoDB");
+  const query = { word: wordName };
+  const results = await db.collection("words").findOne(query);
+  console.log("Results:", results);
+
+  if (!results) res.status(404).send("Not Found");
+  else res.status(200).send(results);
+});
+
+app.post("/listWords", async (req, res) => {
+  try {
+    const { word } = req.body;
+
+    if (!word) {
+      return res
+        .status(400)
+        .send("Bad Request: Missing required field 'word'.");
+    }
+
+    const newDocument = {
+      word: word,
+    };
+
+    const result = await db.collection("words").insertOne(newDocument);
+
+    // Fetch the newly added document including _id
+    const addedWord = await db
+      .collection("words")
+      .findOne({ _id: result.insertedId });
+
+    res.status(201).json(addedWord);
+  } catch (error) {
+    console.error('Error adding word:', error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/listWords/:word", async (req, res) => {
+  const wordName = req.params.word;
+
+  try {
     await client.connect();
-    console.log("Node connected successfully to GET-id MongoDB");
-    const query = {"id": wordid };
 
-    const results = await db.collection("words")
-    .findOne(query);
+    const query = { word: wordName };
+    const result = await db.collection("words").deleteOne(query);
 
-    console.log("Results :", results);
-    if (!results) res.send("Not Found").status(404);
-    else res.send(results).status(200);
+    if (result.deletedCount > 0) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send("Word not found");
+    }
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
-
-app.post("/addWord", async (req, res) => {
-  await client.connect();
-  const keys = Object.keys(req.body);
-  const values = Object.values(req.body);
-
-  const id = values[0]; // id
-  const word = values[1]; // word
-  console.log(id, word);
-  
-  const newDocument = {
-    "id":id,
-    "word":word,
-  };
-  const results = await db.collection("words").insertOne(newDocument);
-  res.status(200);
-  res.send(results);
-});
-  
-app.delete("/deleteWord", async (req, res) => {
-  await client.connect();
-  // const keys = Object.keys(req.body);
-  const values = Object.values(req.body);
-
-  const id = values[0]; // id
-  console.log("word to delete :",id);
-
-  const query = { id: id };
-
-  const results = await db.collection("words").deleteOne(query);
-  res.status(200);
-  res.send(results);
-});
-
-
-app.listen(port, () => {
-  console.log("App listening at http://%s:%s", host, port);
-});
-
